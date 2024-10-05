195a4caf0c9a372e5f931d4f7aa28d09b23a8537
Aditya <bondreaditya99@gmail.com>
2024-10-05 19:40:07 +0530

    Added Currency-Exchange Service

---
+++ currency-exchange/src/main/java/com/microservices/currency_exchange/CurrencyExchangeController.java
@@ -0,0 +1,15 @@
+package com.microservices.currency_exchange;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange currencyExchange(@PathVariable String from,@PathVariable String to){
+        return new CurrencyExchange(1L,"USD","INR", BigDecimal.valueOf(83));
+    }
+}
