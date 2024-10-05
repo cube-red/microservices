8d0f469fab57e7f23b3603dd25fb9d7d87f7e566
Aditya <bondreaditya99@gmail.com>
2024-10-05 23:38:07 +0530

    Added a Currenvy Conversion Service and connected it to the Currency Exchange Service

--- currency-exchange/src/main/java/com/microservices/currency_exchange/CurrencyExchangeController.java
+++ currency-exchange/src/main/java/com/microservices/currency_exchange/CurrencyExchangeController.java
@@ -1,5 +1,6 @@
 package com.microservices.currency_exchange;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,13 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+
+    @Autowired
+    private CurrencyExchangeRepository repository;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange currencyExchange(@PathVariable String from,@PathVariable String to){
-        return new CurrencyExchange(1L,"USD","INR", BigDecimal.valueOf(83));
+        //return new CurrencyExchange(1L,"USD","INR", BigDecimal.valueOf(83));
+        return repository.findByFromAndTo(from,to);
     }
 }
